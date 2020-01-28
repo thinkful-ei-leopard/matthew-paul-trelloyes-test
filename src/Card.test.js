aea14dff1e662b121be885c9c5b1bcd4665f079c
wavinginspace <paulbaisley@gmail.com>
2020-01-28 11:41:38 -0500

    card test

---
+++ src/Card.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import renderer from 'react-test-renderer'; 
+
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<Card />, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+  .create(<Card key='test'
+      title='test'
+      content='test'/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
+
